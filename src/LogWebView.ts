fd26d5ea6a9a18a9e467160083acc08411819009
jp <jp.cpmhd@gmail.com>
2019-03-16 20:33:13 +0100

    time log

--- src/LogWebView.ts
+++ src/LogWebView.ts
@@ -29,7 +29,7 @@ export class LogWebView {
       'time-tracker-log', // Identifies the type of the webview. Used internally
       'Time tracker log', // Title of the panel displayed to the user
       vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-      {enableScripts: true} // Webview options. More on these later.
+      { enableScripts: true } // Webview options. More on these later.
     );
 
     this._panel.webview.html = this.getWebviewContent();
@@ -69,11 +69,6 @@ export class LogWebView {
     return 0;
   }
 
-  // private getTimeIntervals(timeIntervals: TimeInterval[], timeFrom: number, timeTo: number) {
-  //   const result = timeIntervals.filter(x => x.start >= timeFrom && x.start <= timeTo);
-  //   return result;
-  // }
-
   private getItervalsSum(intervals: TimeInterval[]) {
     const timeIntervalsDuration = intervals.map(x => x.end - x.start);
     const sum = timeIntervalsDuration && timeIntervalsDuration.length > 0 ? timeIntervalsDuration.reduce((accumulator, currentValue) => accumulator + currentValue) : 0;
@@ -105,6 +100,9 @@ export class LogWebView {
     const monthTableRows = this.getTimePeriodDateData(timeIntervals, "month", "months", "MMMM", "Month");
     const weekTableRows = this.getTimePeriodDateData(timeIntervals, "week", "weeks", "w", "Week");
 
+    const yearSum = this.getItervalsSum(getTimeIntervalsCroppedToTimeRange(timeIntervals, moment(new Date(year, 1, 1)).startOf('year').valueOf(), moment(new Date(year, 1, 1)).endOf('year').valueOf()));
+    
+
     const monthNames = [];
     const monthMilliseconds = [];
     const startOfyear = moment(new Date(year, 1, 1)).startOf('year');
@@ -119,6 +117,7 @@ export class LogWebView {
     }
 
     return `<h2> ${year} </h2>
+    <b> Total time spent: </b> ${formatTimeFromMiliseconds(yearSum)}
 
     <h3 class="collapsible active"> <span class="arrow-down">⯆</span> <span class="arrow-right">⯈</span> Days</h3>
         <div class="content" style="display:block">
@@ -243,11 +242,12 @@ export class LogWebView {
 
   private getWebviewContent() {
 
+    const timeIntervals = this.getThisYearAllTimeIntervals();
 
     const today = moment().startOf('day');
     const yesterday = today.clone().subtract(1, 'days');
 
-    const timeIntervals = this.getThisYearAllTimeIntervals();
+    const todayMilliseconds = this.getTimeSum(timeIntervals, today.valueOf(), today.clone().add(1, 'days').valueOf());
     const yesterdayMilliseconds = this.getTimeSum(timeIntervals, yesterday.valueOf(), today.valueOf());
     const last7DaysMilliseconds = this.getTimeSum(timeIntervals, today.clone().subtract(7, 'days').valueOf(), today.valueOf());
     const thisWeekMilliseconds = this.getTimeSum(timeIntervals, today.clone().subtract(7, 'days').valueOf(), today.valueOf());
@@ -263,14 +263,12 @@ export class LogWebView {
             <title>Time tracker log</title>
         </head>
         <body>
-
             <h1>Time tracker log</h1>
             <h2>Statistics</h2>
 
             <table class="rtable rtable--flip">
                 <thead>
                     <tr>
-                        <th>Total time</th>
                         <th>Today time</th>
                         <th>Yesterday time</th>
                         <th>This week</th>
@@ -283,8 +281,7 @@ export class LogWebView {
                 </thead>
                 <tbody>
                     <tr>
-                        <td style="min-width: 100px;">${formatTimeFromMiliseconds(this._storage.totalDurationMiliseconds)}</td>
-                        <td style="min-width: 100px;">${formatTimeFromMiliseconds(this._storage.todayDurationMiliseconds)}</td>
+                        <td style="min-width: 100px;">${formatTimeFromMiliseconds(todayMilliseconds)}</td>
                         <td style="min-width: 100px;">${formatTimeFromMiliseconds(yesterdayMilliseconds)}</td>
                         <td style="min-width: 100px;">${formatTimeFromMiliseconds(thisWeekMilliseconds)}</td>
                         <td style="min-width: 100px;">${formatTimeFromMiliseconds(last7DaysMilliseconds)}</td>
@@ -324,196 +321,196 @@ export class LogWebView {
   getStyles() {
 
     const result = `      
-    <style>
+      <style>
 
-    .arrow-down
-    {
-      display:none;
-    }
-
-    .active > .arrow-down
-    {
-        display:inline;
-    }
-
-    .active > .arrow-right
-    {
+      .arrow-down
+      {
         display:none;
-    }
+      }
 
-    .collapsible {
-      cursor: pointer;
-      padding: 10px;
-      padding-left:0px;
-      width: 100%;
-      border: 1px solid bottom;
-      text-align: left;
-      outline: none;
-      font-size: 15px;
-    }
-    
-    .active, .collapsible:hover {
-      font-weight:bold;
-    }
-    
-    .content {
-      display: none;
-      overflow: hidden;
-    }
+      .active > .arrow-down
+      {
+          display:inline;
+      }
 
+      .active > .arrow-right
+      {
+          display:none;
+      }
 
-  .rtable {
-      /*!
-  // IE needs inline-block to position scrolling shadows otherwise use:
-    // display: block;
-    // max-width: min-content;
-    */
-      display: inline-block;
-      vertical-align: top;
-      max-width: 100%;
-
-      overflow-x: auto;
-
-      // optional - looks better for small cell values
-      white-space: nowrap;
-
-      border-collapse: collapse;
-      border-spacing: 0;
-  }
-
-  .rtable,
-  .rtable--flip tbody {
-      // optional - enable iOS momentum scrolling
-      -webkit-overflow-scrolling: touch;
-
-      // scrolling shadows
-      background: radial-gradient(left, ellipse, rgba(0, 0, 0, .2) 0%, rgba(0, 0, 0, 0) 75%) 0 center,
-          radial-gradient(right, ellipse, rgba(0, 0, 0, .2) 0%, rgba(0, 0, 0, 0) 75%) 100% center;
-      background-size: 10px 100%, 10px 100%;
-      background-attachment: scroll, scroll;
-      background-repeat: no-repeat;
-  }
-
-  // change these gradients from white to your background colour if it differs
-  // gradient on the first cells to hide the left shadow
-  .rtable td:first-child,
-  .rtable--flip tbody tr:first-child {
-      background-image: linear-gradient(to right, rgba(255, 255, 255, 1) 50%, rgba(255, 255, 255, 0) 100%);
-      background-repeat: no-repeat;
-      background-size: 20px 100%;
-  }
-
-  // gradient on the last cells to hide the right shadow
-  .rtable td:last-child,
-  .rtable--flip tbody tr:last-child {
-      background-image: linear-gradient(to left, rgba(255, 255, 255, 1) 50%, rgba(255, 255, 255, 0) 100%);
-      background-repeat: no-repeat;
-      background-position: 100% 0;
-      background-size: 20px 100%;
-  }
-
-  .rtable th {
-      font-size: 11px;
-      text-align: left;
-      text-transform: uppercase;
-  }
-
-  .rtable th,
-  .rtable td {
-      padding: 6px 12px;
-      border: 1px solid #d9d7ce;
-  }
-
-  .rtable--flip {
-      display: flex;
-      overflow: hidden;
-      background: none;
-  }
-
-  .rtable--flip thead {
-      display: flex;
-      flex-shrink: 0;
-      min-width: min-content;
-  }
-
-  .rtable--flip tbody {
-      display: flex;
-      position: relative;
-      overflow-x: auto;
-      overflow-y: hidden;
-  }
-
-  .rtable--flip tr {
-      display: flex;
-      flex-direction: column;
-      min-width: min-content;
-      flex-shrink: 0;
-  }
-
-  .rtable--flip td,
-  .rtable--flip th {
-      display: block;
-  }
-
-  .rtable--flip td {
-      background-image: none !important;
-      // border-collapse is no longer active
-      border-left: 0;
-  }
-
-  // border-collapse is no longer active
-  .rtable--flip th:not(:last-child),
-  .rtable--flip td:not(:last-child) {
-      border-bottom: 0;
-  }
-
-  /*!
-  // CodePen house keeping
-  */
-
-  body {
-      margin: 0;
-      padding: 25px;
+      .collapsible {
+        cursor: pointer;
+        padding: 10px;
+        padding-left:0px;
+        width: 100%;
+        border: 1px solid bottom;
+        text-align: left;
+        outline: none;
+        font-size: 15px;
+      }
       
-      font-size: 14px;
-      line-height: 20px;
-  }
+      .active, .collapsible:hover {
+        font-weight:bold;
+      }
+      
+      .content {
+        display: none;
+        overflow: hidden;
+      }
 
-  h1,
-  h2,
-  h3 {
-      margin: 0 0 10px 0;
-      //color: #1d97bf;
-  }
 
-  h1 {
-      font-size: 25px;
-      line-height: 30px;
-  }
+      .rtable {
+          /*!
+      // IE needs inline-block to position scrolling shadows otherwise use:
+        // display: block;
+        // max-width: min-content;
+        */
+          display: inline-block;
+          vertical-align: top;
+          max-width: 100%;
 
-  h2 {
-      font-size: 20px;
-      line-height: 25px;
-  }
+          overflow-x: auto;
 
-  h3 {
-      font-size: 16px;
-      line-height: 20px;
-  }
+          // optional - looks better for small cell values
+          white-space: nowrap;
 
-  table {
-      margin-bottom: 30px;
-  }
+          border-collapse: collapse;
+          border-spacing: 0;
+      }
 
-  a {
-      //color: #ff6680;
-  }
+      .rtable,
+      .rtable--flip tbody {
+          // optional - enable iOS momentum scrolling
+          -webkit-overflow-scrolling: touch;
 
-  code {
-      //background: #fffbcc;
-      font-size: 12px;
-  }
-</style>`;
+          // scrolling shadows
+          background: radial-gradient(left, ellipse, rgba(0, 0, 0, .2) 0%, rgba(0, 0, 0, 0) 75%) 0 center,
+              radial-gradient(right, ellipse, rgba(0, 0, 0, .2) 0%, rgba(0, 0, 0, 0) 75%) 100% center;
+          background-size: 10px 100%, 10px 100%;
+          background-attachment: scroll, scroll;
+          background-repeat: no-repeat;
+      }
+
+      // change these gradients from white to your background colour if it differs
+      // gradient on the first cells to hide the left shadow
+      .rtable td:first-child,
+      .rtable--flip tbody tr:first-child {
+          background-image: linear-gradient(to right, rgba(255, 255, 255, 1) 50%, rgba(255, 255, 255, 0) 100%);
+          background-repeat: no-repeat;
+          background-size: 20px 100%;
+      }
+
+      // gradient on the last cells to hide the right shadow
+      .rtable td:last-child,
+      .rtable--flip tbody tr:last-child {
+          background-image: linear-gradient(to left, rgba(255, 255, 255, 1) 50%, rgba(255, 255, 255, 0) 100%);
+          background-repeat: no-repeat;
+          background-position: 100% 0;
+          background-size: 20px 100%;
+      }
+
+      .rtable th {
+          font-size: 11px;
+          text-align: left;
+          text-transform: uppercase;
+      }
+
+      .rtable th,
+      .rtable td {
+          padding: 6px 12px;
+          border: 1px solid #d9d7ce;
+      }
+
+      .rtable--flip {
+          display: flex;
+          overflow: hidden;
+          background: none;
+      }
+
+      .rtable--flip thead {
+          display: flex;
+          flex-shrink: 0;
+          min-width: min-content;
+      }
+
+      .rtable--flip tbody {
+          display: flex;
+          position: relative;
+          overflow-x: auto;
+          overflow-y: hidden;
+      }
+
+      .rtable--flip tr {
+          display: flex;
+          flex-direction: column;
+          min-width: min-content;
+          flex-shrink: 0;
+      }
+
+      .rtable--flip td,
+      .rtable--flip th {
+          display: block;
+      }
+
+      .rtable--flip td {
+          background-image: none !important;
+          // border-collapse is no longer active
+          border-left: 0;
+      }
+
+      // border-collapse is no longer active
+      .rtable--flip th:not(:last-child),
+      .rtable--flip td:not(:last-child) {
+          border-bottom: 0;
+      }
+
+      /*!
+      // CodePen house keeping
+      */
+
+      body {
+          margin: 0;
+          padding: 25px;
+          
+          font-size: 14px;
+          line-height: 20px;
+      }
+
+      h1,
+      h2,
+      h3 {
+          margin: 0 0 10px 0;
+          //color: #1d97bf;
+      }
+
+      h1 {
+          font-size: 25px;
+          line-height: 30px;
+      }
+
+      h2 {
+          font-size: 20px;
+          line-height: 25px;
+      }
+
+      h3 {
+          font-size: 16px;
+          line-height: 20px;
+      }
+
+      table {
+          margin-bottom: 30px;
+      }
+
+      a {
+          //color: #ff6680;
+      }
+
+      code {
+          //background: #fffbcc;
+          font-size: 12px;
+      }
+    </style>`;
 
     return result;
   }
